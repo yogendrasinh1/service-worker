df1efa864fa562b5a24fbe337d313f817d6ca3f3
Spiral <72080730+oakideadeveloper@users.noreply.github.com>
2024-03-12 15:18:49 +0530

    registered

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         test abhi
+         test new
         </p>
         <a
           className="App-link"
